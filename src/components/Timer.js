ab284d8d18554237c07fe48dd2c2752453fa692b
Moisés Mariano <moisesduartem@gmail.com>
2020-09-17 15:13:10 -0300

    testa adicao de id e classe

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,7 +1,7 @@
 import El from './../El.js';
 
 const Timer = () => (
-  new El('h1', 'Cronometro')
+  new El('h1', 'Cronometro', { id: 'timer', class: 'word black' })
 );
 
 export default Timer;
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,5 +1,5 @@
 import El from './../El.js';
 
-const Title = () => new El('h3', 'El - Construa SPAs em JavaScript');
+const Title = () => new El('h3', 'El - Construa SPAs em JavaScript', { id: 'title' });
 
 export default Title;
