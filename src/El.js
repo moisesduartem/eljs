cbe7fd840001c38c7857ad5d8d75749bcf156d7c
Moisés Mariano <moisesduartem@gmail.com>
2020-09-17 01:31:48 -0300

    separa fn createElement de El

--- src/El.js
+++ src/El.js
@@ -1,24 +1,31 @@
+/**
+ * [Cria elemento utilizando a API da DOM]
+ * @param {*} element 
+ * @param {*} content 
+ */
+const createElement = function (element, content) {
+  if (!(typeof element === 'object')) {
+    element = document.createElement(element);
+  }
+
+  if (typeof content === 'object') {
+    element.appendChild(content);
+    return element;
+  }
+
+  const text = document.createTextNode(content);
+  element.appendChild(text);
+  return element;
+}
+
 const El = function (element, content) {
+
+  console.log(typeof content, content);
 
   this.element = element;
   this.content = content;
 
-  this.createElement = function () {
-    if (!(typeof this.element === 'object')) {
-      this.element = document.createElement(this.element);
-    }
-
-    if (typeof content === 'object') {
-      this.element.appendChild(this.content);
-      return this.element;
-    }
-
-    const text = document.createTextNode(this.content);
-    this.element.appendChild(text);
-    return this.element;
-  }
-
-  return this.createElement();
+  return createElement(this.element, this.content);
 };
 
 export default El;
