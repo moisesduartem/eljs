8e7eecdbe16cd59571c02981b28e6a0ec94be944
Moisés Mariano <moisesduartem@gmail.com>
2020-09-17 02:06:44 -0300

    passa mais de um elemento

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import El from './El.js';
 import Title from './components/Title.js';
+import Timer from './components/Timer.js';
 
 const App = () => (
-  Title()
+  new El(
+    'div',
+    [
+      Title(),
+      Timer()
+    ]
+  )
 );
 
 export default App;
